5539f19bf167653b02f1dc652217bf42cde36521
Garikai <gijimaga@grinnell.edu>
2024-04-07 17:19:28 -0500

    Block class complete

--- src/Block.java
+++ src/Block.java
@@ -1,3 +1,4 @@
+import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -13,16 +14,147 @@ public class Block {
   // +--------+----------------------------------------------------------
   // | Fields |
   // +--------+
-
-  
+ 
+  int blockNum;
+  int amountTransferred; 
+  long nonce;
+  Hash prevHash;
+  Hash currHash;
 
   // +--------------+----------------------------------------------------
   // | Constructors |
   // +--------------+
+  /* Block(int num, int amount, Hash prevHash)
+   * creates a new block from the specified parameters, performing the mining operation 
+   * to discover the nonce and hash for this block given these parameters.
+   */
+  public Block(int num, int amount, Hash prevHash) {
+    this.blockNum = num;
+    this.amountTransferred = amount;
+    this.prevHash = prevHash;
+    
+    try {
+      mineBlock();
+    }
+    catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+  } // Block(int num, int amount, Hash prevHash)
 
 
+  /* Block(int num, int amount, Hash prevHash, long nonce)
+   * creates a new block from the specified parameters, using the provided nonce and 
+   * additional parameters to generate the hash for the block. Because the nonce is 
+   * provided, this constructor does not need to perform the mining operation; 
+   * it can compute the hash directly.
+   */
+  public Block(int num, int amount, Hash prevHash, long nonce) {
+    this.blockNum = num;
+    this.amountTransferred = amount;
+    this.prevHash = prevHash;
+    this.nonce = nonce;
+    
+    String textToHash = "" + this.amountTransferred + (this.prevHash == null ? "" : prevHash.toString()) + nonce;
+    byte[] bytesToHash = textToHash.getBytes();
+    try {
+      this.currHash = new Hash(bytesToHash);
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    } // try-catch
+  } // Block(int num, int amount, Hash prevHash, long nonce)
   // +---------------+---------------------------------------------------
   // |    Methods    |
   // +---------------+
+
+  /* 
+   * int getNum():
+   * returns the number of this block.
+  */
+  public int getNum() {
+    return this.blockNum;
+  }
+
+  /* 
+   * int getAmount(): 
+   * returns the amount transferred that is recorded in this block
+  */
+  public int getAmount() {
+    return this.amountTransferred;
+  }
+
+  /* 
+   * long getNonce(): 
+   * returns the nonce of this block.
+  */
+  public long getNonce() {
+    return this.nonce;
+  }
+
+  /* 
+   * Hash getPrevHash(): 
+   * returns the hash of the previous block in the blockchain.
+  */
+  public Hash getPrevHash() {
+    return this.prevHash;
+  }
+
+  /* 
+   * Hash getHash(): 
+   * returns the hash of this block.
+  */
+  public Hash getHash() {
+    return this.currHash;
+  }
+
+  /*
+   * String toString(): 
+   * returns a string representation of the block as below :
+   * Block <num> (Amount: <amt>, Nonce: <nonce>, prevHash: <prevHash>, hash: <hash>)
+   */
+  public String toString() {
+    return String.format("Block %d (Amount: %d, Nonce: %d, prevHash: %s, hash: %s)",
+                        this.blockNum, this.amountTransferred, this.nonce, 
+                        this.prevHash.toString(), this.currHash.toString());
+  }
+
+
  
+  // +---------------------+---------------------------------------------------
+  // |   Private Methods   |
+  // +---------------------+
+
+  /* void mineBlock()
+   * mines block
+  */
+  private void mineBlock() throws NoSuchAlgorithmException {
+    this.nonce = 0;
+
+    while(this.nonce <= Long.MAX_VALUE) {
+      makeHash();
+
+      if(this.currHash.isValid()) {
+        return;
+      } // if
+      this.nonce++;
+    } // while
+  } // mineBlock
+
+  private void makeHash() throws NoSuchAlgorithmException {
+    MessageDigest md = MessageDigest.getInstance("sha-256");
+
+    byte[] blockNumBytes = ByteBuffer.allocate(Integer.BYTES).putInt(this.blockNum).array();
+    byte[] amtTransBytes = ByteBuffer.allocate(Integer.BYTES).putInt(this.amountTransferred).array();
+    byte[] nonceBytes = ByteBuffer.allocate(Long.BYTES).putLong(this.nonce).array();
+
+    md.update(blockNumBytes);
+    md.update(amtTransBytes);
+    md.update(nonceBytes);
+
+    if (this.prevHash != null) {
+      md.update(this.prevHash.getData());
+    }
+    this.currHash = new Hash(md.digest());
+  }
 }
+ 
+
--- src/Hash.java
+++ src/Hash.java
@@ -14,26 +14,34 @@ public class Hash {
   // | Fields |
   // +--------+
   private byte[] hash;
-  private byte[] initial_data;
   // +--------------+----------------------------------------------------
   // | Constructors |
   // +--------------+
+  /*
+   * Hash(byte[] data)
+   * constructs a new Hash object that contains the given hash (as an array of bytes).
+   */
   public Hash(byte[] data) throws NoSuchAlgorithmException{
-    this.initial_data = data;
-    MessageDigest md = MessageDigest.getInstance("sha-256");
-    md.update(data);
-    this.hash = md.digest();
-  }
+    this.hash = data;
+  } //Hash(byte[] data)
 
   
 
   // +---------------+---------------------------------------------------
   // |    Methods    |
   // +---------------+
+
+  /* byte[] getData()
+   * returns the hash contained in this object.
+   */
   public byte[] getData() {
     return this.hash;
-  }
-
+  }// byte[] getData()
+ 
+  /* boolean isValid()
+   *  returns true if this hash meets the criteria for validity,
+   *  i.e., its first three indices contain zeroes.
+   */
   public boolean isValid() {
     for(int i = 0; i < 3; i++) {
       if(hash[i] != 0) {
@@ -41,8 +49,12 @@ public class Hash {
       } // if
     } // for-loop
     return true;
-  }
-
+  } //boolean isValid()
+ 
+  /* String toString()
+   *   returns the string representation of the hash as 
+   * a string of hexadecimal digits, 2 digits per byte.
+   */
   public String toString() {
     int[] hashInts = new int[this.hash.length];
     for (int i = 0; i < hashInts.length; i++) {
@@ -53,13 +65,17 @@ public class Hash {
         result += String.format("%02X", hashInts[i]);
     }//for-loop
     return result;
-  }
+  } //String toString()
 
+
+  /* boolean equals(Object other)
+   *  returns true if this hash is structurally equal to the argument.
+   */
   public boolean equals (Object other) {
     if(other instanceof Hash) {
         Hash o = (Hash) other;
         return Arrays.equals(o.hash, this.hash);
     }
     return false;
-  }
+  } // boolean equals(Object other)
 }
